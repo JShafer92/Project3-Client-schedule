6c5454993c0610f88464d1ac826b01bdaefb2229
JShafer92 <ptwraith1@gmail.com>
2018-09-23 18:17:23 -0700

    first commit

---
+++ project3/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from './components/Pages/about/about'
+import './App.css';
+
+const App = () => (
+  <Router>
+    <div>
+      <Navbar />
+      <Wrapper>
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/calender" component={calender} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/login" component={Login} />
+      </Wrapper>
+      <Footer />
+    </div>
+  </Router>
+);
+
+export default App;
