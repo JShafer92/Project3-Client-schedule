45964c5cf756018a2aff63d9691422c0735f8ceb
JShafer92 <ptwraith1@gmail.com>
2018-09-25 19:01:54 -0700

    changed about page

--- project3/src/App.js
+++ project3/src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from './components/Pages/about/about'
 import './App.css';
-
+import Navbar from './components/Nav/index'
 const App = () => (
   <Router>
     <div>
       <Navbar />
-      <Wrapper>
+      {/* <Wrapper> */}
         <Route exact path="/" component={About} />
         <Route exact path="/about" component={About} />
-        <Route exact path="/calender" component={calender} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/login" component={Login} />
-      </Wrapper>
-      <Footer />
+        {/* <Route exact path="/calender" component={calender} /> */}
+        {/* <Route exact path="/profile" component={Profile} /> */}
+        {/* <Route exact path="/login" component={Login} /> */}
+      {/* </Wrapper> */}
+      {/* <Footer /> */}
     </div>
   </Router>
 );
--- project3/src/components/Nav/navbar.js
+++ project3/src/components/Nav/navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+// import "./Navbar.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 const Navbar = props => (
